e82934cd55ffe3077391e6d6d8ef193289b54305
Andevaldo Vitório <andevaldo.vitorio@icomp.ufam.edu.br>
2023-09-30 13:28:01 -0400

    feat: add teacher entity

---
+++ src/app/backend/entities/Teacher.java
@@ -0,0 +1,69 @@
+package app.backend.entities;
+
+import java.sql.Date;
+
+public class Teacher {
+	private String name;
+	private Date birthDay;
+	private int indenticatorNumber;
+	private String email;
+	private String phone;
+	private String trainingArea;
+	private int yearsOfExperience;
+
+	public void setBirthDay(Date birthDay) {
+		this.birthDay = birthDay;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public void setIndenticatorNumber(int indenticatorNumber) {
+		this.indenticatorNumber = indenticatorNumber;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public void setTrainingArea(String trainingArea) {
+		this.trainingArea = trainingArea;
+	}
+
+	public void setYearsOfExperience(int yearsOfExperience) {
+		this.yearsOfExperience = yearsOfExperience;
+	}
+
+	public Date getBirthDay() {
+		return birthDay;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public int getIndenticatorNumber() {
+		return indenticatorNumber;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public String getTrainingArea() {
+		return trainingArea;
+	}
+
+	public int getYearsOfExperience() {
+		return yearsOfExperience;
+	}
+}
