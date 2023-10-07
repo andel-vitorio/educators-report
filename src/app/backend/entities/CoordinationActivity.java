1f5a5040741a937e81be141cb99b347d2271c771
Andevaldo Vitório <andevaldo.vitorio@icomp.ufam.edu.br>
2023-10-07 13:13:53 -0400

    style: formatting entity classes

--- src/app/backend/entities/CoordinationActivity.java
+++ src/app/backend/entities/CoordinationActivity.java
@@ -6,174 +6,174 @@ import java.time.LocalDate;
  * Classe que representa uma atividade de coordenação.
  */
 public class CoordinationActivity {
-    private String activityTitle; // Título da atividade.
-    private String nameOfPersonResponsible; // Nome da pessoa responsável pela atividade.
-    private LocalDate startDate; // Data de início da atividade.
-    private LocalDate endDate; // Data de término da atividade.
-    private String priority; // Prioridade da atividade.
-    private String status; // Status da atividade.
-    private String description; // Descrição da atividade.
-    private int id; // Identificador único para a atividade.
+  private String activityTitle; // Título da atividade.
+  private String nameOfPersonResponsible; // Nome da pessoa responsável pela atividade.
+  private LocalDate startDate; // Data de início da atividade.
+  private LocalDate endDate; // Data de término da atividade.
+  private String priority; // Prioridade da atividade.
+  private String status; // Status da atividade.
+  private String description; // Descrição da atividade.
+  private int id; // Identificador único para a atividade.
 
-    /**
-     * Define o identificador único para a atividade.
-     *
-     * @param id O identificador único.
-     */
-    public void setId(int id) {
-        this.id = id;
-    }
+  /**
+   * Define o identificador único para a atividade.
+   *
+   * @param id O identificador único.
+   */
+  public void setId(int id) {
+    this.id = id;
+  }
 
-    /**
-     * Obtém o identificador único para a atividade.
-     *
-     * @return O identificador único.
-     */
-    public int getId() {
-        return id;
-    }
+  /**
+   * Obtém o identificador único para a atividade.
+   *
+   * @return O identificador único.
+   */
+  public int getId() {
+    return id;
+  }
 
-    /**
-     * Define o título da atividade.
-     *
-     * @param activityTitle O título da atividade.
-     */
-    public void setActivityTitle(String activityTitle) {
-        this.activityTitle = activityTitle;
-    }
+  /**
+   * Define o título da atividade.
+   *
+   * @param activityTitle O título da atividade.
+   */
+  public void setActivityTitle(String activityTitle) {
+    this.activityTitle = activityTitle;
+  }
 
-    /**
-     * Define a descrição da atividade.
-     *
-     * @param description Uma descrição da atividade.
-     */
-    public void setDescription(String description) {
-        this.description = description;
-    }
+  /**
+   * Define a descrição da atividade.
+   *
+   * @param description Uma descrição da atividade.
+   */
+  public void setDescription(String description) {
+    this.description = description;
+  }
 
-    /**
-     * Define a data de término da atividade.
-     *
-     * @param endDate A data de término da atividade.
-     */
-    public void setEndDate(LocalDate endDate) {
-        this.endDate = endDate;
-    }
+  /**
+   * Define a data de término da atividade.
+   *
+   * @param endDate A data de término da atividade.
+   */
+  public void setEndDate(LocalDate endDate) {
+    this.endDate = endDate;
+  }
 
-    /**
-     * Define o nome da pessoa responsável pela atividade.
-     *
-     * @param nameOfPersonResponsible O nome da pessoa responsável.
-     */
-    public void setNameOfPersonResponsible(String nameOfPersonResponsible) {
-        this.nameOfPersonResponsible = nameOfPersonResponsible;
-    }
+  /**
+   * Define o nome da pessoa responsável pela atividade.
+   *
+   * @param nameOfPersonResponsible O nome da pessoa responsável.
+   */
+  public void setNameOfPersonResponsible(String nameOfPersonResponsible) {
+    this.nameOfPersonResponsible = nameOfPersonResponsible;
+  }
 
-    /**
-     * Define a prioridade da atividade.
-     *
-     * @param priority A prioridade da atividade.
-     */
-    public void setPriority(String priority) {
-        this.priority = priority;
-    }
+  /**
+   * Define a prioridade da atividade.
+   *
+   * @param priority A prioridade da atividade.
+   */
+  public void setPriority(String priority) {
+    this.priority = priority;
+  }
 
-    /**
-     * Define a data de início da atividade.
-     *
-     * @param startDate A data de início da atividade.
-     */
-    public void setStartDate(LocalDate startDate) {
-        this.startDate = startDate;
-    }
+  /**
+   * Define a data de início da atividade.
+   *
+   * @param startDate A data de início da atividade.
+   */
+  public void setStartDate(LocalDate startDate) {
+    this.startDate = startDate;
+  }
 
-    /**
-     * Define o status da atividade.
-     *
-     * @param status O status da atividade.
-     */
-    public void setStatus(String status) {
-        this.status = status;
-    }
+  /**
+   * Define o status da atividade.
+   *
+   * @param status O status da atividade.
+   */
+  public void setStatus(String status) {
+    this.status = status;
+  }
 
-    /**
-     * Obtém o título da atividade.
-     *
-     * @return O título da atividade.
-     */
-    public String getActivityTitle() {
-        return activityTitle;
-    }
+  /**
+   * Obtém o título da atividade.
+   *
+   * @return O título da atividade.
+   */
+  public String getActivityTitle() {
+    return activityTitle;
+  }
 
-    /**
-     * Obtém a descrição da atividade.
-     *
-     * @return Uma descrição da atividade.
-     */
-    public String getDescription() {
-        return description;
-    }
+  /**
+   * Obtém a descrição da atividade.
+   *
+   * @return Uma descrição da atividade.
+   */
+  public String getDescription() {
+    return description;
+  }
 
-    /**
-     * Obtém a data de término da atividade.
-     *
-     * @return A data de término da atividade.
-     */
-    public LocalDate getEndDate() {
-        return endDate;
-    }
+  /**
+   * Obtém a data de término da atividade.
+   *
+   * @return A data de término da atividade.
+   */
+  public LocalDate getEndDate() {
+    return endDate;
+  }
 
-    /**
-     * Obtém o nome da pessoa responsável pela atividade.
-     *
-     * @return O nome da pessoa responsável.
-     */
-    public String getNameOfPersonResponsible() {
-        return nameOfPersonResponsible;
-    }
+  /**
+   * Obtém o nome da pessoa responsável pela atividade.
+   *
+   * @return O nome da pessoa responsável.
+   */
+  public String getNameOfPersonResponsible() {
+    return nameOfPersonResponsible;
+  }
 
-    /**
-     * Obtém a prioridade da atividade.
-     *
-     * @return A prioridade da atividade.
-     */
-    public String getPriority() {
-        return priority;
-    }
+  /**
+   * Obtém a prioridade da atividade.
+   *
+   * @return A prioridade da atividade.
+   */
+  public String getPriority() {
+    return priority;
+  }
 
-    /**
-     * Obtém a data de início da atividade.
-     *
-     * @return A data de início da atividade.
-     */
-    public LocalDate getStartDate() {
-        return startDate;
-    }
+  /**
+   * Obtém a data de início da atividade.
+   *
+   * @return A data de início da atividade.
+   */
+  public LocalDate getStartDate() {
+    return startDate;
+  }
 
-    /**
-     * Obtém o status da atividade.
-     *
-     * @return O status da atividade.
-     */
-    public String getStatus() {
-        return status;
-    }
+  /**
+   * Obtém o status da atividade.
+   *
+   * @return O status da atividade.
+   */
+  public String getStatus() {
+    return status;
+  }
 
-    /**
-     * Retorna uma representação em string do objeto CoordinationActivity.
-     *
-     * @return Uma representação em string do objeto.
-     */
-    @Override
-    public String toString() {
-        return "CoordinationActivity{" +
-                "activityTitle='" + activityTitle + '\'' +
-                ", nameOfPersonResponsible='" + nameOfPersonResponsible + '\'' +
-                ", startDate=" + startDate +
-                ", endDate=" + endDate +
-                ", priority='" + priority + '\'' +
-                ", status='" + status + '\'' +
-                ", description='" + description + '\'' +
-                '}';
-    }
+  /**
+   * Retorna uma representação em string do objeto CoordinationActivity.
+   *
+   * @return Uma representação em string do objeto.
+   */
+  @Override
+  public String toString() {
+    return "CoordinationActivity{" +
+        "activityTitle='" + activityTitle + '\'' +
+        ", nameOfPersonResponsible='" + nameOfPersonResponsible + '\'' +
+        ", startDate=" + startDate +
+        ", endDate=" + endDate +
+        ", priority='" + priority + '\'' +
+        ", status='" + status + '\'' +
+        ", description='" + description + '\'' +
+        '}';
+  }
 }
--- src/app/backend/entities/Teacher.java
+++ src/app/backend/entities/Teacher.java
@@ -6,174 +6,174 @@ import java.time.LocalDate;
  * Classe que representa um professor.
  */
 public class Teacher {
-    private String name; // Nome do professor.
-    private LocalDate birthDay; // Data de nascimento do professor.
-    private String indentificatorNumber; // Número de identificação do professor.
-    private String email; // Email do professor.
-    private String phone; // Número de telefone do professor.
-    private String trainingArea; // Área de formação do professor.
-    private int yearsOfExperience; // Anos de experiência do professor.
-    private int id; // Identificador único para o professor.
+  private String name; // Nome do professor.
+  private LocalDate birthDay; // Data de nascimento do professor.
+  private String indentificatorNumber; // Número de identificação do professor.
+  private String email; // Email do professor.
+  private String phone; // Número de telefone do professor.
+  private String trainingArea; // Área de formação do professor.
+  private int yearsOfExperience; // Anos de experiência do professor.
+  private int id; // Identificador único para o professor.
 
-    /**
-     * Define a data de nascimento do professor.
-     *
-     * @param birthDay A data de nascimento do professor.
-     */
-    public void setBirthDay(LocalDate birthDay) {
-        this.birthDay = birthDay;
-    }
+  /**
+   * Define a data de nascimento do professor.
+   *
+   * @param birthDay A data de nascimento do professor.
+   */
+  public void setBirthDay(LocalDate birthDay) {
+    this.birthDay = birthDay;
+  }
 
-    /**
-     * Define o email do professor.
-     *
-     * @param email O email do professor.
-     */
-    public void setEmail(String email) {
-        this.email = email;
-    }
+  /**
+   * Define o email do professor.
+   *
+   * @param email O email do professor.
+   */
+  public void setEmail(String email) {
+    this.email = email;
+  }
 
-    /**
-     * Define o número de identificação do professor.
-     *
-     * @param indenticatorNumber O número de identificação do professor.
-     */
-    public void setIndentificatorNumber(String indenticatorNumber) {
-        this.indentificatorNumber = indenticatorNumber;
-    }
+  /**
+   * Define o número de identificação do professor.
+   *
+   * @param indenticatorNumber O número de identificação do professor.
+   */
+  public void setIndentificatorNumber(String indenticatorNumber) {
+    this.indentificatorNumber = indenticatorNumber;
+  }
 
-    /**
-     * Define o nome do professor.
-     *
-     * @param name O nome do professor.
-     */
-    public void setName(String name) {
-        this.name = name;
-    }
+  /**
+   * Define o nome do professor.
+   *
+   * @param name O nome do professor.
+   */
+  public void setName(String name) {
+    this.name = name;
+  }
 
-    /**
-     * Define o número de telefone do professor.
-     *
-     * @param phone O número de telefone do professor.
-     */
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
+  /**
+   * Define o número de telefone do professor.
+   *
+   * @param phone O número de telefone do professor.
+   */
+  public void setPhone(String phone) {
+    this.phone = phone;
+  }
 
-    /**
-     * Define a área de formação do professor.
-     *
-     * @param trainingArea A área de formação do professor.
-     */
-    public void setTrainingArea(String trainingArea) {
-        this.trainingArea = trainingArea;
-    }
+  /**
+   * Define a área de formação do professor.
+   *
+   * @param trainingArea A área de formação do professor.
+   */
+  public void setTrainingArea(String trainingArea) {
+    this.trainingArea = trainingArea;
+  }
 
-    /**
-     * Define os anos de experiência do professor.
-     *
-     * @param yearsOfExperience Os anos de experiência do professor.
-     */
-    public void setYearsOfExperience(int yearsOfExperience) {
-        this.yearsOfExperience = yearsOfExperience;
-    }
+  /**
+   * Define os anos de experiência do professor.
+   *
+   * @param yearsOfExperience Os anos de experiência do professor.
+   */
+  public void setYearsOfExperience(int yearsOfExperience) {
+    this.yearsOfExperience = yearsOfExperience;
+  }
 
-    /**
-     * Define o identificador único para o professor.
-     *
-     * @param id O identificador único.
-     */
-    public void setId(int id) {
-        this.id = id;
-    }
+  /**
+   * Define o identificador único para o professor.
+   *
+   * @param id O identificador único.
+   */
+  public void setId(int id) {
+    this.id = id;
+  }
 
-    /**
-     * Obtém a data de nascimento do professor.
-     *
-     * @return A data de nascimento do professor.
-     */
-    public LocalDate getBirthDay() {
-        return birthDay;
-    }
+  /**
+   * Obtém a data de nascimento do professor.
+   *
+   * @return A data de nascimento do professor.
+   */
+  public LocalDate getBirthDay() {
+    return birthDay;
+  }
 
-    /**
-     * Obtém o email do professor.
-     *
-     * @return O email do professor.
-     */
-    public String getEmail() {
-        return email;
-    }
+  /**
+   * Obtém o email do professor.
+   *
+   * @return O email do professor.
+   */
+  public String getEmail() {
+    return email;
+  }
 
-    /**
-     * Obtém o número de identificação do professor.
-     *
-     * @return O número de identificação do professor.
-     */
-    public String getIndentificatorNumber() {
-        return indentificatorNumber;
-    }
+  /**
+   * Obtém o número de identificação do professor.
+   *
+   * @return O número de identificação do professor.
+   */
+  public String getIndentificatorNumber() {
+    return indentificatorNumber;
+  }
 
-    /**
-     * Obtém o nome do professor.
-     *
-     * @return O nome do professor.
-     */
-    public String getName() {
-        return name;
-    }
+  /**
+   * Obtém o nome do professor.
+   *
+   * @return O nome do professor.
+   */
+  public String getName() {
+    return name;
+  }
 
-    /**
-     * Obtém o número de telefone do professor.
-     *
-     * @return O número de telefone do professor.
-     */
-    public String getPhone() {
-        return phone;
-    }
+  /**
+   * Obtém o número de telefone do professor.
+   *
+   * @return O número de telefone do professor.
+   */
+  public String getPhone() {
+    return phone;
+  }
 
-    /**
-     * Obtém a área de formação do professor.
-     *
-     * @return A área de formação do professor.
-     */
-    public String getTrainingArea() {
-        return trainingArea;
-    }
+  /**
+   * Obtém a área de formação do professor.
+   *
+   * @return A área de formação do professor.
+   */
+  public String getTrainingArea() {
+    return trainingArea;
+  }
 
-    /**
-     * Obtém os anos de experiência do professor.
-     *
-     * @return Os anos de experiência do professor.
-     */
-    public int getYearsOfExperience() {
-        return yearsOfExperience;
-    }
+  /**
+   * Obtém os anos de experiência do professor.
+   *
+   * @return Os anos de experiência do professor.
+   */
+  public int getYearsOfExperience() {
+    return yearsOfExperience;
+  }
 
-    /**
-     * Obtém o identificador único do professor.
-     *
-     * @return O identificador único.
-     */
-    public int getId() {
-        return id;
-    }
+  /**
+   * Obtém o identificador único do professor.
+   *
+   * @return O identificador único.
+   */
+  public int getId() {
+    return id;
+  }
 
-    /**
-     * Retorna uma representação em string do objeto Teacher.
-     *
-     * @return Uma representação em string do objeto.
-     */
-    @Override
-    public String toString() {
-        return "Teacher{" +
-                "name='" + name + '\'' +
-                ", birthDay=" + birthDay +
-                ", indenticatorNumber='" + indentificatorNumber + '\'' +
-                ", email='" + email + '\'' +
-                ", phone='" + phone + '\'' +
-                ", trainingArea='" + trainingArea + '\'' +
-                ", yearsOfExperience=" + yearsOfExperience +
-                '}';
-    }
+  /**
+   * Retorna uma representação em string do objeto Teacher.
+   *
+   * @return Uma representação em string do objeto.
+   */
+  @Override
+  public String toString() {
+    return "Teacher{" +
+        "name='" + name + '\'' +
+        ", birthDay=" + birthDay +
+        ", indenticatorNumber='" + indentificatorNumber + '\'' +
+        ", email='" + email + '\'' +
+        ", phone='" + phone + '\'' +
+        ", trainingArea='" + trainingArea + '\'' +
+        ", yearsOfExperience=" + yearsOfExperience +
+        '}';
+  }
 }
