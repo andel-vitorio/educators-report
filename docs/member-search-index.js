f5435b2019b00498f45fb9dc3040317a268b011f
Andevaldo Vitório <andevaldo.vitorio@icomp.ufam.edu.br>
2023-10-07 14:48:39 -0400

    docs: generate javadocs

--- docs/member-search-index.js
+++ docs/member-search-index.js
@@ -1,1 +1,1 @@
-memberSearchIndex = [{"p":"<Unnamed>","c":"App","l":"App()","u":"%3Cinit%3E()"},{"p":"<Unnamed>","c":"App","l":"changeComponentColors(Container, Color, Color)","u":"changeComponentColors(java.awt.Container,java.awt.Color,java.awt.Color)"},{"p":"<Unnamed>","c":"App","l":"main(String[])","u":"main(java.lang.String[])"}];updateSearchResults();
+memberSearchIndex = [{"p":"<Unnamed>","c":"App","l":"App()","u":"%3Cinit%3E()"},{"p":"<Unnamed>","c":"App","l":"main(String[])","u":"main(java.lang.String[])"}];updateSearchResults();
